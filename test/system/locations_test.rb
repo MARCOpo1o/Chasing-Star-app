1f6cf771efc25a0c72efb33f8183e02f41231bfc
JeremyHeJian <kaonimaoba@gmail.com>
2022-09-23 15:31:27 -0400

    add location table

---
+++ test/system/locations_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class LocationsTest < ApplicationSystemTestCase
+  setup do
+    @location = locations(:one)
+  end
+
+  test "visiting the index" do
+    visit locations_url
+    assert_selector "h1", text: "Locations"
+  end
+
+  test "should create location" do
+    visit locations_url
+    click_on "New location"
+
+    fill_in "Coordinates", with: @location.coordinates
+    fill_in "Location name", with: @location.location_name
+    click_on "Create Location"
+
+    assert_text "Location was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Location" do
+    visit location_url(@location)
+    click_on "Edit this location", match: :first
+
+    fill_in "Coordinates", with: @location.coordinates
+    fill_in "Location name", with: @location.location_name
+    click_on "Update Location"
+
+    assert_text "Location was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Location" do
+    visit location_url(@location)
+    click_on "Destroy this location", match: :first
+
+    assert_text "Location was successfully destroyed"
+  end
+end
