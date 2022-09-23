8a66fd24fe2a6f8fae32eacafb6c634bd371686a
JeremyHeJian <kaonimaoba@gmail.com>
2022-09-23 15:51:07 -0400

    bundle lock --add-platform x86_64-linux

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_193144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "hello_worlds", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "locations", force: :cascade do |t|
     t.string "location_name"
     t.json "coordinates"
