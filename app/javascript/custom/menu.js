85ae1cfc883068d4865b4371762fa24592338130
Jingqian Cheng <jingqiancheng1998@gmail.com>
2022-10-21 15:52:28 -0400

    adjust navbar, created location card

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,12 +2,6 @@
 
 // Add toggle listeners to listen for clicks.
 document.addEventListener("turbo:load", function() {
-    let hamburger = document.querySelector("#hamburger");
-    hamburger.addEventListener("click", function(event) {
-      event.preventDefault();
-      let menu = document.querySelector("#navbar-menu");
-      menu.classList.toggle("collapse");
-     });
 
     let account = document.querySelector("#account");
     account.addEventListener("click", function(event) {
@@ -16,10 +10,5 @@ document.addEventListener("turbo:load", function() {
       menu.classList.toggle("active");
     });
 
-    let navbarNavDropdown = document.querySelector("#navbarNavDropdown");
-    navbarNavDropdown.addEventListener("click", function(event) {
-      event.preventDefault();
-      let menu = document.querySelector("#navbar-menu");
-      menu.classList.toggle("collapse");
-    });
+    
 });
