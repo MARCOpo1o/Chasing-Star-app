527af29047912eba04bb6653ce34108af6a4ecfb
Jingqian Cheng <jingqiancheng1998@gmail.com>
2022-10-24 09:56:06 -0400

    fix some known bugs

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class MapsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get maps_index_url
+    get maps_url
     assert_response :success
   end
 end
